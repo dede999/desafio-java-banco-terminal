b3c1330705cf5530988b6206c739541ab1b4240b
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-09-11 01:09:55 -0300

    Apply BankAccount class to the main file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import com.example.bank.BankAccount;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BankAccount account = BankAccount.accountCreateFromTerminal();
+        System.out.println(account);
+        account.deposit(25.00);
+        account.withdraw(25, true);
+        account.withdraw(20);
     }
 }
